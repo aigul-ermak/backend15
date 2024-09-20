20fcee6f3456f138133e595c1d6ea0c49b673779
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-19 22:03:56 -0300

    Modified jwtConstants: added for refresh token

--- src/features/auth/constants.ts
+++ src/features/auth/constants.ts
@@ -1,12 +1,16 @@
 import * as process from "process";
 
-export const jwtConstants = {
-    jwr_secret: process.env.JWT_SECRET,
+export const jwtAccessConstants = {
+    jwt_secret: process.env.JWT_SECRET,
     access_token_expiry: process.env.ACCESS_TOKEN_EXPIRY,
-    refresh_token_expiry: process.env.REFRESH_TOKEN_EXPIRY
+};
+
+export const jwtRefreshConstants = {
+    jwt_secret: process.env.JWT_SECRET,
+    refresh_token_expiry: process.env.REFRESH_TOKEN_EXPIRY,
 };
 
 export const basicConstants = {
     userName: process.env.HTTP_BASIC_USER,
-    password: process.env.HTTP_BASIC_PASS
+    password: process.env.HTTP_BASIC_PASS,
 }
