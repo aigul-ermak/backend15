a6f022f15fbee186eb2001eb11b981c018e25348
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-22 22:41:29 -0300

    Updated use case: CreateLikeForPostUseCase, added type fo command.likeStatus

--- src/features/usecases/createLikeForPostUseCase.ts
+++ src/features/usecases/createLikeForPostUseCase.ts
@@ -8,13 +8,17 @@ import {JwtService} from "@nestjs/jwt";
 import {LikesRepository} from "../like/infrastructure/likes.repository";
 import {UsersQueryRepository} from "../users/infrastructure/users.query-repository";
 import {LikesQueryRepository} from "../like/infrastructure/likes.query-repository";
+import {LikeStatusInputDto} from "../like/api/model/like-status.input.dto";
+import {LIKE_STATUS} from "../like/domain/like.entity";
 
 
 export class CreateLikeForPostUseCaseCommand {
-
-    constructor(public postId: string, public likeStatus: any, userId: string) {
+    constructor(public postId: string,
+                public likeStatus: LikeStatusInputDto,
+                public userId: string) {
     }
 }
+
 
 @CommandHandler(CreateLikeForPostUseCaseCommand)
 export class CreateLikeForPostUseCase implements ICommandHandler<CreateLikeForPostUseCaseCommand> {
@@ -31,6 +35,7 @@ export class CreateLikeForPostUseCase implements ICommandHandler<CreateLikeForPo
     async execute(command: CreateLikeForPostUseCaseCommand) {
 
         const post = await this.postsQueryRepository.getPostById(command.postId);
+        let like;
 
         if (post === null) {
             throw new NotFoundException(`Post not found`);
@@ -42,41 +47,43 @@ export class CreateLikeForPostUseCase implements ICommandHandler<CreateLikeForPo
             throw new BadRequestException();
         }
 
-        const isLikeExist = await this.likeQueryRepository.checkLikeStatus(user.id, post.id);
+        const isLikeExist = await this.likeQueryRepository.checkLike(user.id, post.id);
 
         if (!isLikeExist) {
+
             const newLike = {
-                status: likeStatus,
-                userId,
-                parentId,
-                login: user?.accountData.login,
+                status: command.likeStatus,
+                userId: command.userId,
+                parentId: command.postId,
+                login: user?.login,
                 createdAt: Date.now(),
             };
 
             const res = await this.likeRepository.createLike(newLike);
 
-            if (likeStatus == LIKE_STATUS.LIKE) {
-                await this.postsRepository.incrementLikeCount(parentId);
-            } else if (likeStatus === LIKE_STATUS.DISLIKE) {
-                await this.postsRepository.incrementDislikeCount(parentId);
+            if ((command.likeStatus as unknown as LIKE_STATUS) == LIKE_STATUS.LIKE) {
+                await this.postsRepository.incrementLikeCount(command.postId,);
+            } else if ((command.likeStatus as unknown as LIKE_STATUS) === LIKE_STATUS.DISLIKE) {
+                await this.postsRepository.incrementDislikeCount(command.postId,);
             }
             return res;
         } else {
-            like = await this.likeRepository.getLike(parentId, userId);
-            if (like!.status !== likeStatus) {
+
+            like = await this.likeQueryRepository.getLike(command.postId, command.userId);
+            if (like!.status !== command.likeStatus) {
                 if (like!.status === LIKE_STATUS.LIKE) {
-                    await this.postsRepository.decrementLikeCount(parentId);
+                    await this.postsRepository.decrementLikeCount(command.postId,);
                 } else if (like!.status === LIKE_STATUS.DISLIKE) {
-                    await this.postsRepository.decrementDislikeCount(parentId);
+                    await this.postsRepository.decrementDislikeCount(command.postId,);
                 }
 
-                if (likeStatus === LIKE_STATUS.LIKE) {
-                    await this.postsRepository.incrementLikeCount(parentId);
-                } else if (likeStatus === LIKE_STATUS.DISLIKE) {
-                    await this.postsRepository.incrementDislikeCount(parentId);
+                if ((command.likeStatus as unknown as LIKE_STATUS) === LIKE_STATUS.LIKE) {
+                    await this.postsRepository.incrementLikeCount(command.postId,);
+                } else if ((command.likeStatus as unknown as LIKE_STATUS) === LIKE_STATUS.DISLIKE) {
+                    await this.postsRepository.incrementDislikeCount(command.postId,);
                 }
             }
-            like!.status = likeStatus;
+            like!.status = command.likeStatus;
 
             return await this.likeRepository.updateLike(like!._id.toString(), like);
         }
