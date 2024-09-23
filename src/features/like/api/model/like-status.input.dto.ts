ae30dcd8f3d02b697f71fcc4c599ce440286dc31
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-22 22:42:03 -0300

    Refactor LikeStatus.input.dto

--- src/features/like/api/model/like-status.input.dto.ts
+++ src/features/like/api/model/like-status.input.dto.ts
@@ -3,5 +3,5 @@ import {LIKE_STATUS} from "../../domain/like.entity";
 
 export class LikeStatusInputDto {
     @IsEnum(LIKE_STATUS, {message: 'Status must be one of Like, Dislike, or None'})
-    status: LIKE_STATUS;
+    likeStatus: LIKE_STATUS;
 }
