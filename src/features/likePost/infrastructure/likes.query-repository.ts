35364e3a7e9f2674e618d5113c69600b0673f800
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-25 15:32:31 -0300

    Updated getNewestLikesForPost method

--- src/features/likePost/infrastructure/likes.query-repository.ts
+++ src/features/likePost/infrastructure/likes.query-repository.ts
@@ -13,11 +13,22 @@ export class LikesQueryRepository {
         return await this.likeModel.findOne({parentId: parentId, userId: userId});
     }
 
-    async getNewestLikes(parentId: string) {
-        return this.likeModel.find({parentId: parentId, status: LIKE_STATUS.LIKE})
+    async getNewestLikesForPost(postId: string) {
+        // return this.likeModel.find({parentId: parentId, status: LIKE_STATUS.LIKE})
+        //     .sort({createdAt: -1})
+        //     .limit(3);
+        const newestLikes = await this.likeModel.find({parentId: postId, status: LIKE_STATUS.LIKE})
             .sort({createdAt: -1})
-            .limit(3);
+            .limit(3)
+            .lean();
+
+        return newestLikes.map(like => ({
+            addedAt: like.createdAt,
+            userId: like.userId,
+            login: like.login,
+        }));
     }
+
 
     async checkLike({parentId, userId}: { parentId: string, userId: string }) {
         const res = await this.likeModel.findOne({parentId: parentId, userId: userId}).lean();
