efc816a77ecef270fa8d56a462cc7c902219cf43
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-25 10:48:53 -0300

    Created create comment for post use case

---
+++ src/features/usecases/createCommentForPostUseCase.ts
@@ -0,0 +1,66 @@
+import {CommandHandler, ICommandHandler} from "@nestjs/cqrs";
+import {BadRequestException, NotFoundException} from "@nestjs/common";
+import {PostsQueryRepository} from "../posts/infrastructure/posts.query-repository";
+import {UsersQueryRepository} from "../users/infrastructure/users.query-repository";
+import {CreateCommentInputDto} from "../comments/api/model/input/create-comment.input.dto";
+import {CommentsRepository} from "../comments/infrastructure/comments.repository";
+import {PostCommentOutputModelMapper} from "../posts/api/models/output/post-comment.output.model";
+import {CommentsQueryRepository} from "../comments/infrastructure/comments.query-repository";
+
+
+export class CreateCommentForPostUseCaseCommand {
+    constructor(public postId: string,
+                public userId: string,
+                public comment: CreateCommentInputDto) {
+    }
+}
+
+@CommandHandler(CreateCommentForPostUseCaseCommand)
+export class CreateCommentForPostUseCase implements ICommandHandler<CreateCommentForPostUseCaseCommand> {
+
+    constructor(
+        private commentRepository: CommentsRepository,
+        private commentQueryRepository: CommentsQueryRepository,
+        private postsQueryRepository: PostsQueryRepository,
+        private usersQueryRepository: UsersQueryRepository,
+    ) {
+    }
+
+    async execute(command: CreateCommentForPostUseCaseCommand) {
+
+        const post = await this.postsQueryRepository.getPostById(command.postId);
+        let like;
+
+        if (post === null) {
+            throw new NotFoundException(`Post not found`);
+        }
+
+        const user = await this.usersQueryRepository.getUserById(command.userId);
+
+        if (!user) {
+            throw new BadRequestException();
+        }
+
+// TODO type?
+        const newComment = {
+            posId: command.postId,
+            content: command.comment.content,
+            commentatorInfo: {
+                userId: user.id,
+                userLogin: user.login
+            },
+            createdAt: new Date().toISOString(),
+            likesInfo: {
+                likesCount: 0,
+                dislikesCount: 0,
+                myStatus: "None"
+            }
+        }
+
+        const commentId = await this.commentRepository.createComment(newComment);
+
+        const postComment = await this.commentQueryRepository.getCommentById(commentId);
+
+        return PostCommentOutputModelMapper(postComment);
+    }
+}
