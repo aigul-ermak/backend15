0266aa55b1ea1790962e453fe4bdb2093f004d5d
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-24 22:00:28 -0300

    Refactor code: removed imports

--- src/features/posts/posts.module.ts
+++ src/features/posts/posts.module.ts
@@ -1,5 +1,4 @@
 import {Module} from '@nestjs/common';
-import {PostsController} from './api/posts.controller';
 import {PostsService} from './application/posts.service';
 import {MongooseModule} from '@nestjs/mongoose';
 import {PostsRepository} from './infrastructure/posts.repository';
@@ -8,7 +7,7 @@ import {Blog, BlogEntity} from '../blogs/domain/blog.entity';
 import {BlogsRepository} from '../blogs/infrastructure/blogs.repository';
 import {PostsQueryRepository} from "./infrastructure/posts.query-repository";
 import {CqrsModule} from "@nestjs/cqrs";
-import {LikesModule} from "../likePost/likes.module";
+
 
 @Module({
     imports: [
