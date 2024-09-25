265c3835dcfd08f9be42566e9b24894023c0fe84
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-25 13:29:54 -0300

    Refactor code: renamed dto

--- src/features/comments/api/model/input/comment-input.dto.ts
+++ src/features/comments/api/model/input/comment-input.dto.ts
@@ -1,7 +1,7 @@
 import {IsString, Length} from "class-validator";
 import {Trim} from "../../../../../infrastructure/decorators/transform/trim";
 
-export class CreateCommentInputDto {
+export class CommentInputDto {
     @IsString()
     @Trim()
     @Length(20, 300, {message: "Length not correct"})
