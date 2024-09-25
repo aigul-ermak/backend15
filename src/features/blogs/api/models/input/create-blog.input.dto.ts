a4d39ea8fcb5e62854db698cbe20174625d40774
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-25 13:28:14 -0300

    Updated CreatePostToBlogDto: added schema restrictions

--- src/features/blogs/api/models/input/create-blog.input.dto.ts
+++ src/features/blogs/api/models/input/create-blog.input.dto.ts
@@ -20,8 +20,19 @@ export class CreateBlogInputDto {
 }
 
 export class CreatePostToBlogDto {
+    @IsString()
+    @Trim()
+    @Length(1, 30, {message: "Length not correct"})
     title: string;
+
+    @IsString()
+    @Trim()
+    @Length(1, 100, {message: "Length not correct"})
     shortDescription: string;
+
+    @IsString()
+    @Trim()
+    @Length(1, 1000, {message: "Length not correct"})
     content: string;
 }
 
