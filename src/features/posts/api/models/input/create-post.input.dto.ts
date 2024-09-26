5ca98a629b3ed9919b1743fb10d946e51f4ee7fb
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-26 16:03:09 -0300

    Added dto : CreatePostForBlogInputDto

--- src/features/posts/api/models/input/create-post.input.dto.ts
+++ src/features/posts/api/models/input/create-post.input.dto.ts
@@ -1,5 +1,6 @@
-import {IsString, Length} from "class-validator";
+import {IsString, IsUUID, Length} from "class-validator";
 import {Trim} from "../../../../../infrastructure/decorators/transform/trim";
+import {ValidateBlogExists} from "../../../../../infrastructure/decorators/validate/blog-is-exist.decorator";
 
 export class CreatePostInputDto {
     @IsString()
@@ -17,8 +18,31 @@ export class CreatePostInputDto {
     @Length(1, 1000, {message: "Length not correct"})
     content: string;
 
+    // @IsString()
+    // @Trim()
+    @ValidateBlogExists({message: "Blog not correct"})
+    blogId: string;
+}
+
+export class CreatePostForBlogInputDto {
     @IsString()
     @Trim()
+    @Length(1, 30, {message: "Length not correct"})
+    title: string;
+
+    @IsString()
+    @Trim()
+    @Length(1, 100, {message: "Length not correct"})
+    shortDescription: string;
+
+    @IsString()
+    @Trim()
+    @Length(1, 1000, {message: "Length not correct"})
+    content: string;
+
+    @IsString()
+    @Trim()
+        //@ValidateBlogExists({message: "Blog not correct"})
     blogId: string;
 }
 
