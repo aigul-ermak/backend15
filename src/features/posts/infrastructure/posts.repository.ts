47ec01315b9e56345296d5139944e09928db3139
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-26 17:41:56 -0300

    Added new method: updatePostLikesCount

--- src/features/posts/infrastructure/posts.repository.ts
+++ src/features/posts/infrastructure/posts.repository.ts
@@ -3,6 +3,7 @@ import {InjectModel} from '@nestjs/mongoose';
 import {Model} from 'mongoose';
 import {Post, PostDocument} from '../domain/posts.entity';
 import {UpdatePostDto} from '../api/models/input/create-post.input.dto';
+import {UpdatePostLikesCountDto} from "../api/models/input/create-postLikesCount.input.dto";
 
 @Injectable()
 export class PostsRepository {
@@ -22,6 +23,12 @@ export class PostsRepository {
     async update(id: string, updatePostDto: UpdatePostDto) {
         return this.postModel
             .findByIdAndUpdate(id, updatePostDto, {new: true})
+            .exec();
+    }
+
+    async updatePostLikesCount(id: string, updatePostLikesCountDto: UpdatePostLikesCountDto) {
+        return this.postModel
+            .findByIdAndUpdate(id, updatePostLikesCountDto, {new: true})
             .exec();
     }
 
