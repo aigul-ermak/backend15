3d738be738900798279629e622c7ac05ea53757b
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-26 15:23:50 -0300

    Added decorator: BlogExistsConstraint

---
+++ src/infrastructure/decorators/validate/blog-is-exist.decorator.ts
@@ -0,0 +1,38 @@
+import {
+    ValidatorConstraint,
+    ValidatorConstraintInterface,
+    ValidationArguments,
+    registerDecorator,
+    ValidationOptions
+} from 'class-validator';
+import {Injectable} from '@nestjs/common';
+import {BlogsQueryRepository} from "../../../features/blogs/infrastructure/blogs.query-repository";
+
+
+@ValidatorConstraint({async: true})
+@Injectable()
+export class BlogExistsConstraint implements ValidatorConstraintInterface {
+    constructor(private readonly blogsQueryRepository: BlogsQueryRepository) {
+    }
+
+    async validate(blogId: string, args: ValidationArguments) {
+        const blog = await this.blogsQueryRepository.getBlogById(blogId);
+        return !!blog;
+    }
+
+    defaultMessage(args: ValidationArguments) {
+        return 'Blog with the given ID does not exist';
+    }
+}
+
+export function ValidateBlogExists(validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            constraints: [],
+            validator: BlogExistsConstraint,
+        });
+    };
+}
