e6d6519235f4f1cb2fff326fc9a3d3f14bcf6d6a
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-27 16:30:59 -0300

    Updated test for Posts

--- test/posts.spec.ts
+++ test/posts.spec.ts
@@ -4,7 +4,6 @@ import {INestApplication} from '@nestjs/common';
 const request = require('supertest');
 import {AppModule} from './../src/app.module';
 import {applyAppSettings} from "../src/settings/apply.app.setting";
-import {BlogOutputModelMapper} from "../src/features/blogs/api/models/output/blog.output.model";
 
 
 const HTTP_BASIC_USER = process.env.HTTP_BASIC_USER as string;
@@ -15,7 +14,7 @@ const getBasicAuthHeader = (username: string, password: string) => {
     return `Basic ${base64Credentials}`;
 };
 
-let blog;
+let blog, post, user, accessToken;
 
 describe('Posts testing', () => {
     let app: INestApplication;
@@ -74,35 +73,324 @@ describe('Posts testing', () => {
     });
 
 
-    it('return 201 for create blog', async () => {
+    it('return 201 for create post', async () => {
 
-        const blogDto = {
-            name: 'testBlog',
-            description: 'testDescription',
-            websiteUrl: 'https://hEO9ArXY2EqnGG_jmMb9Yi8zRBjLabLGWMR9e.yiKejrxeCGMhNvmCqzmaOm_Fv_jf.5ahBsb1mXVdXbyt9KYo8l907V'
+        const postDto = {
+            title: 'testPost',
+            shortDescription: 'testShortDescription',
+            content: 'testContent',
+            blogId: blog.id
         };
 
         const response = await request(httpServer)
-            .post('/blogs')
+            .post('/posts')
             .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
-            .send(blogDto)
+            .send(postDto)
             .expect(201);
 
 
         const expectedResult = {
             id: expect.any(String),
-            name: blogDto.name,
-            description: expect.any(String),
-            websiteUrl: expect.any(String),
+            title: postDto.title,
+            shortDescription: expect.any(String),
+            content: expect.any(String),
+            blogId: blog.id,
+            blogName: blog.name,
             createdAt: expect.any(String),
-            isMembership: false
+            extendedLikesInfo: {
+                likesCount: 0,
+                dislikesCount: 0,
+                myStatus: "None",
+                newestLikes: []
+            }
+
         };
 
-        blog = response.body;
+        post = response.body;
+        expect(response.body).toEqual(expectedResult);
+
+    });
+
+    it('returns 400 for invalid post fields', async () => {
+
+        const postDto = {
+            title: '',
+            shortDescription: '',
+            content: '',
+            blogId: ''
+        };
+
+        const response = await request(httpServer)
+            .post('/posts')
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .send(postDto)
+            .expect(400);
+
+
+        const expectedResult = {
+            "errorsMessages": [
+                {
+                    "message": "Length not correct",
+                    "field": "title"
+                },
+                {
+                    "message": "ShortDescription not correct",
+                    "field": "shortDescription"
+                },
+                {
+                    "message": "Content not correct",
+                    "field": "content"
+                },
+            ]
+        };
+
+        expect(response.body).toEqual(expectedResult);
+    });
+
+    it('returns 401 for Unauthorized', async () => {
+
+        const postDto = {
+            title: 'testPost',
+            shortDescription: 'testShortDescription',
+            content: 'testContent',
+            blogId: blog.id
+        };
+
+        const response = await request(httpServer)
+            .post('/posts')
+            .send(postDto)
+            .expect(401);
+
+        expect(response.body).toEqual({
+            message: 'Unauthorized',
+            statusCode: 401
+        });
+    });
+
+    it('return 200 for get post', async () => {
+
+        const postId = post.id;
+
+        const response = await request(httpServer)
+            .get(`/posts/${postId}`)
+            .expect(200);
+
+
+        const expectedResult = {
+            id: expect.any(String),
+            title: post.title,
+            shortDescription: expect.any(String),
+            content: expect.any(String),
+            blogId: blog.id,
+            blogName: blog.name,
+            createdAt: expect.any(String),
+            extendedLikesInfo: {
+                likesCount: 0,
+                dislikesCount: 0,
+                myStatus: "None",
+                newestLikes: []
+            }
+
+        };
+
+        expect(response.body).toEqual(expectedResult);
+
+    });
+
+    it('returns 404 for not found post', async () => {
+
+        const postId = '66f6a26adc4b81ea41af73f8';
+
+        const response = await request(httpServer)
+            .get(`/posts/${postId}`)
+            .expect(404);
+
+        expect(response.body).toEqual({
+            statusCode: 404,
+            message: 'Post not found',
+            error: 'Not Found'
+        });
+    });
+
+    it('return 204 for update post', async () => {
+
+        const postId = post.id;
+
+        const postUpdateDto = {
+            title: 'testUpdatePost',
+            shortDescription: 'testUpdateShortDescription',
+            content: 'testUpdateContent',
+            blogId: blog.id
+        };
+
+        const response = await request(httpServer)
+            .put(`/posts/${postId}`)
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .send(postUpdateDto)
+            .expect(204);
+
+    });
+
+    it('returns 400 for invalid post updated fields', async () => {
+
+        const postId = post.id;
+
+        const postUpdateDto = {
+            title: '',
+            shortDescription: '',
+            content: '',
+            blogId: ''
+        };
+
+
+        const response = await request(httpServer)
+            .put(`/posts/${postId}`)
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .send(postUpdateDto)
+            .expect(400);
+
+
+        const expectedResult = {
+            "errorsMessages": [
+                {
+                    "message": "Length not correct",
+                    "field": "title"
+                },
+                {
+                    "message": "ShortDescription not correct",
+                    "field": "shortDescription"
+                },
+                {
+                    "message": "Content not correct",
+                    "field": "content"
+                },
+            ]
+        };
+
+        expect(response.body).toEqual(expectedResult);
+
+    });
+
+    it('returns 401 for Unauthorized', async () => {
+
+        const postId = post.id;
+
+        const postUpdateDto = {
+            title: 'testUpdatePost',
+            shortDescription: 'testUpdateShortDescription',
+            content: 'testUpdateContent',
+            blogId: blog.id
+        };
+
+        const response = await request(httpServer)
+            .put(`/posts/${postId}`)
+            .send(postUpdateDto)
+            .expect(401);
+
+        expect(response.body).toEqual({
+            message: 'Unauthorized',
+            statusCode: 401
+        });
+    });
+
+    it('return 201 for create user', async () => {
+
+        const userDto = {
+            login: "user1",
+            password: "password",
+            email: "example@example.com"
+        };
+
+        const response = await request(httpServer)
+            .post(`/users`)
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .send(userDto)
+            .expect(201);
+
+
+        const expectedResult = {
+            id: expect.any(String),
+            login: userDto.login,
+            email: userDto.email,
+            createdAt: expect.any(String),
+        };
+
+        user = response.body;
 
         expect(response.body).toEqual(expectedResult);
 
     });
 
 
+    it('return 200 for login user', async () => {
+
+
+        const userLoginDto = {
+            loginOrEmail: "user1",
+            password: "password"
+        };
+
+
+        const response = await request(httpServer)
+            .post(`/auth/login`)
+            .send(userLoginDto)
+            .expect(200);
+
+
+        const expectedResult = {
+            accessToken: expect.any(String)
+        };
+
+        accessToken = response.body.accessToken;
+
+        expect(response.body).toEqual(expectedResult);
+
+    });
+
+    it('returns 204 for add like to post', async () => {
+
+        const postId = post.id;
+
+        const likeDto = {
+            likeStatus: 'Like'
+        };
+
+        const response = await request(httpServer)
+            .put(`/posts/${postId}/like-status`)
+            .set('Authorization', `Bearer ${accessToken}`)
+            .send(likeDto)
+            .expect(204);
+
+
+    });
+
+
+    it('returns 204 for delete post', async () => {
+
+        const postId = post.id;
+
+        const response = await request(httpServer)
+            .delete(`/posts/${postId}`)
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .expect(204);
+    });
+
+    it('returns 201 for delete blog: unauthorised user', async () => {
+
+        const postId = post.id;
+
+        const response = await request(httpServer)
+            .delete(`/posts/${postId}`)
+            .expect(401);
+    });
+
+    it('returns 404 for delete blog: Not found', async () => {
+
+        const postId = '66f6a26adc4b81ea41af73f8';
+
+        const response = await request(httpServer)
+            .delete(`/posts/${postId}`)
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .expect(404);
+    });
 })
