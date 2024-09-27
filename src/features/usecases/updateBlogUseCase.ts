2f46077a8ade6bbd495e77a56672440f07828170
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-27 13:16:39 -0300

    Updated UpdateBlogUseCase: added check validation for blog

--- src/features/usecases/updateBlogUseCase.ts
+++ src/features/usecases/updateBlogUseCase.ts
@@ -1,21 +1,33 @@
 import {BlogsRepository} from "../blogs/infrastructure/blogs.repository";
 import {CommandHandler, ICommandHandler} from "@nestjs/cqrs";
 import {UpdateBlogDto} from "../blogs/api/models/input/update-blog.input.dto";
+import {BlogsQueryRepository} from "../blogs/infrastructure/blogs.query-repository";
+import {NotFoundException} from "@nestjs/common";
 
 
 export class UpdateBlogUseCaseCommand {
-    constructor(public id: string, public updateBlogDto: UpdateBlogDto) {
+    constructor(public blogId: string, public updateBlogDto: UpdateBlogDto) {
     }
 }
 
-
 @CommandHandler(UpdateBlogUseCaseCommand)
 export class UpdateBlogUseCase implements ICommandHandler<UpdateBlogUseCaseCommand> {
-    constructor(private blogsRepository: BlogsRepository) {
+    constructor(
+        private blogsRepository: BlogsRepository,
+        private blogsQueryRepository: BlogsQueryRepository,
+    ) {
     }
 
     async execute(command: UpdateBlogUseCaseCommand) {
-        const {id, updateBlogDto} = command;
-        return await this.blogsRepository.update(id, updateBlogDto);
+
+        const blog = await this.blogsQueryRepository.getBlogById(command.blogId);
+
+        if (!blog) {
+            throw new NotFoundException(`Blog not found`);
+        }
+
+        const {blogId, updateBlogDto} = command;
+
+        return await this.blogsRepository.update(blogId, updateBlogDto);
     }
 }
