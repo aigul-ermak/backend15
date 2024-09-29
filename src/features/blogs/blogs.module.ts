1da4606933d2c9204776328f256509108e79356e
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-29 09:04:15 -0300

    Updated blog module: added provider IsBlogByIdExistsConstraint

--- src/features/blogs/blogs.module.ts
+++ src/features/blogs/blogs.module.ts
@@ -5,13 +5,14 @@ import {BlogsRepository} from './infrastructure/blogs.repository';
 import {MongooseModule} from '@nestjs/mongoose';
 import {Blog, BlogEntity} from './domain/blog.entity';
 import {BlogsQueryRepository} from "./infrastructure/blogs.query-repository";
+import {IsBlogByIdExistsConstraint} from "../../infrastructure/decorators/validation/blog-is-exist.decorator";
 
 
 @Module({
     imports: [
         MongooseModule.forFeature([{name: Blog.name, schema: BlogEntity}]),
     ],
-    providers: [BlogsService, BlogsRepository, BlogsQueryRepository],
+    providers: [BlogsService, BlogsRepository, BlogsQueryRepository, IsBlogByIdExistsConstraint],
     //controllers: [BlogsController],
     exports: [BlogsRepository, BlogsQueryRepository]
 })
