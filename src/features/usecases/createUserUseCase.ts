dc39ba02fbc5e350e95cf49bbae0e08ed1d10a42
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-30 12:08:48 -0300

    Updated CreateUserUseCase: added command bus

--- src/features/usecases/createUserUseCase.ts
+++ src/features/usecases/createUserUseCase.ts
@@ -3,35 +3,42 @@ import {UsersRepository} from "../users/infrastructure/users.repository";
 import {UsersQueryRepository} from "../users/infrastructure/users.query-repository";
 import bcrypt from "bcrypt";
 import {UserDBModel} from "../users/api/models/input/user-db.input.model";
+import {CreatePostForBlogInputDto} from "../posts/api/models/input/create-post.input.dto";
+import {CommandHandler, ICommandHandler} from "@nestjs/cqrs";
+import {CreatePostUseCaseCommand} from "./createPostUseCase";
+import {CreateUserDto} from "../users/api/models/input/create-user.input.dto";
 
-@Injectable()
-export class CreateUserUseCase {
+
+export class CreateUserUseCaseCommand {
+
+    constructor(public createUserDto: CreateUserDto) {
+    }
+}
+
+@CommandHandler(CreateUserUseCaseCommand)
+export class CreateUserUseCase implements ICommandHandler<CreateUserUseCaseCommand> {
     constructor(
         private usersRepository: UsersRepository,
         private usersQueryRepository: UsersQueryRepository,
     ) {
     }
 
-    async execute(
-        email: string,
-        login: string,
-        password: string,
-    ) {
+    async execute(command: CreateUserUseCaseCommand) {
 
-        const existingUser = await this.usersQueryRepository.findOneByEmail(email);
+        const existingUser = await this.usersQueryRepository.findOneByEmail(command.createUserDto.email);
 
         if (existingUser) {
             throw new ConflictException(`User with this email already exists`);
         }
 
         const saltRounds: number = 10;
-        const passwordHashed: string = await bcrypt.hash(password, saltRounds);
+        const passwordHashed: string = await bcrypt.hash(command.createUserDto.password, saltRounds);
 
 
         const newUser: UserDBModel = {
             accountData: {
-                login: login,
-                email: email,
+                login: command.createUserDto.login,
+                email: command.createUserDto.email,
                 passwordHash: passwordHashed,
                 passwordRecoveryCode: "",
                 recoveryCodeExpirationDate: null,
@@ -44,7 +51,10 @@ export class CreateUserUseCase {
                 isConfirmed: false
             }
         }
-        return await this.usersRepository.createUser(newUser);
+        const userId = await this.usersRepository.createUser(newUser);
+
+        return await this.usersQueryRepository.getUserById(userId);
     }
+
 
 }
