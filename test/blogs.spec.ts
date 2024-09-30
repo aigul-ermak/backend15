22829d13203be96a2d9809f55e1a9305e6ecae59
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-30 09:57:41 -0300

    Updated tests for blogs

--- test/blogs.spec.ts
+++ test/blogs.spec.ts
@@ -15,7 +15,7 @@ const getBasicAuthHeader = (username: string, password: string) => {
     return `Basic ${base64Credentials}`;
 };
 
-let blog, blog1;
+let blog, blog1, post, post1;
 
 describe('Blogs testing', () => {
     let app: INestApplication;
@@ -212,8 +212,6 @@ describe('Blogs testing', () => {
             .send(blogUpdateDto)
             .expect(204);
 
-        //blog = response.body;
-
     });
 
     it('returns 400 for invalid blog updated fields', async () => {
@@ -298,6 +296,203 @@ describe('Blogs testing', () => {
         });
     });
 
+    it('return 201 for create post for blog', async () => {
+
+        const blogId = blog.id;
+
+        const postDto = {
+            title: 'testPost',
+            shortDescription: 'testShortDescription',
+            content: 'testContent',
+        };
+
+        const createdPost = {
+            ...postDto,
+            blogId
+        }
+
+        const response = await request(httpServer)
+            .post(`/blogs/${blogId}/posts`)
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .send(createdPost)
+            .expect(201);
+
+
+        const expectedResult = {
+            id: expect.any(String),
+            title: postDto.title,
+            shortDescription: expect.any(String),
+            content: expect.any(String),
+            blogId: blog.id,
+            blogName: expect.any(String),
+            createdAt: expect.any(String),
+            extendedLikesInfo: {
+                likesCount: 0,
+                dislikesCount: 0,
+                myStatus: "None",
+                newestLikes: []
+            }
+        };
+
+        post = response.body;
+        expect(response.body).toEqual(expectedResult);
+
+    });
+
+    it('return 400 for create post for blog', async () => {
+
+        const blogId = blog.id;
+
+        const postDto = {
+            title: '',
+            shortDescription: '',
+            content: '',
+        };
+
+        const createdPost = {
+            ...postDto,
+            blogId
+        }
+
+        const response = await request(httpServer)
+            .post(`/blogs/${blogId}/posts`)
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .send(createdPost)
+            .expect(400);
+
+
+        const expectedResult = {
+            "errorsMessages": [
+                {
+                    "message": "Length not correct",
+                    "field": "title"
+                },
+                {
+                    "message": "ShortDescription not correct",
+                    "field": "shortDescription"
+                },
+                {
+                    "message": "Content not correct",
+                    "field": "content"
+                },
+            ]
+        };
+
+        expect(response.body).toEqual(expectedResult);
+
+    });
+
+    it('return 401 for create post for blog', async () => {
+
+        const blogId = blog.id;
+
+        const postDto = {
+            title: '',
+            shortDescription: '',
+            content: '',
+        };
+
+        const createdPost = {
+            ...postDto,
+            blogId
+        }
+
+        const response = await request(httpServer)
+            .post(`/blogs/${blogId}/posts`)
+            .send(createdPost)
+            .expect(401);
+
+        expect(response.body).toEqual({
+            message: 'Unauthorized',
+            statusCode: 401
+        });
+    });
+
+    it('return 404 for create post for blog', async () => {
+
+        const blogId = '66f94610f417e0eecf048a08';
+
+        const postDto = {
+            title: 'testPost',
+            shortDescription: 'testShortDescription',
+            content: 'testContent',
+        };
+
+        const createdPost = {
+            ...postDto,
+            blogId
+        }
+
+
+        const response = await request(httpServer)
+            .post(`/blogs/${blogId}/posts`)
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .send(postDto)
+            .expect(404);
+
+        expect(response.body).toEqual({
+            statusCode: 404,
+            message: 'Blog not found',
+            error: 'Not Found',
+        });
+    });
+
+    it('return 200 for get posts for blog', async () => {
+
+        const blogId = blog.id;
+
+        const response = await request(httpServer)
+            .get(`/blogs/${blogId}/posts`)
+            //.set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .expect(200);
+
+
+        const expectedResult = {
+            pagesCount: 1,
+            page: 1,
+            pageSize: 10,
+            totalCount: 1,
+            items: [
+                {
+                    id: expect.any(String),
+                    title: post.title,
+                    shortDescription: expect.any(String),
+                    content: expect.any(String),
+                    blogId: blog.id,
+                    blogName: expect.any(String),
+                    createdAt: expect.any(String),
+                    extendedLikesInfo: {
+                        likesCount: 0,
+                        dislikesCount: 0,
+                        myStatus: "None",
+                        newestLikes: []
+                    },
+                },
+            ]
+        };
+
+
+        expect(response.body).toEqual(expectedResult);
+
+    });
+
+    it('return 404 for get posts for blog', async () => {
+
+        const blogId = '66f94610f417e0eecf048a08';
+
+        const response = await request(httpServer)
+            .get(`/blogs/${blogId}/posts`)
+            .set('Authorization', getBasicAuthHeader(HTTP_BASIC_USER, HTTP_BASIC_PASS))
+            .expect(404);
+
+
+        expect(response.body).toEqual({
+            statusCode: 404,
+            message: 'Blog not found',
+            error: 'Not Found',
+        });
+
+    });
 
     it('returns 200 for get all blogs', async () => {
 
@@ -314,7 +509,7 @@ describe('Blogs testing', () => {
             items: [
                 {
                     id: expect.any(String),
-                    name: blog.name,
+                    name: blog1.name,
                     description: expect.any(String),
                     websiteUrl: expect.any(String),
                     createdAt: expect.any(String),
@@ -322,7 +517,7 @@ describe('Blogs testing', () => {
                 },
                 {
                     id: expect.any(String),
-                    name: blog1.name,
+                    name: expect.any(String),
                     description: expect.any(String),
                     websiteUrl: expect.any(String),
                     createdAt: expect.any(String),
@@ -333,24 +528,6 @@ describe('Blogs testing', () => {
 
         expect(response.body).toEqual(expectedResponse);
     });
-
-
-    // it('Get all blogs', async () => {
-    //     const response = await request(httpServer)
-    //         .get('/blogs')
-    //         .expect(200);
-    //
-    //     const expectedResponse = {
-    //         pagesCount: 0,
-    //         page: 1,
-    //         pageSize: 10,
-    //         totalCount: 0,
-    //         items: []
-    //     };
-    //
-    //     expect(response.body).toEqual(expectedResponse);
-    // });
-    //
 
     it('returns 204 for delete blog', async () => {
 
@@ -370,6 +547,7 @@ describe('Blogs testing', () => {
             .delete(`/blogs/${blogId}`)
             .expect(401);
     });
+
     it('returns 404 for delete blog: Not found', async () => {
 
         const blogId = '66f6a26adc4b81ea41af73f8';
